bdd6a0371351972c68a473f9fcbaa4fdae49c2a9
Rizvan <rizvankurungattil@gmail.com>
2023-01-12 03:02:42 +0530

    added filtration

--- MasaiVaccination/Scripts/dashboard.js
+++ MasaiVaccination/Scripts/dashboard.js
@@ -1,75 +1,144 @@
-let userData=JSON.parse(localStorage.getItem("user-data"))||[];
-let vaccinatedUsers=JSON.parse(localStorage.getItem("vaccinated"))||[];
-let tbody=document.getElementById("dash_Board")
-function showData(){
-    tbody.innerHTML=null;
-    userData.forEach(function (user,index){
-        let tr=document.createElement("tr");
-        let id=document.createElement("td");
-        let Name=document.createElement("td");
-        let age=document.createElement("td");
-        let designation=document.createElement("td");
-        let priority=document.createElement("td");
-        let vType=document.createElement("td");
-        let otp=document.createElement("td");
-        let vaccine=document.createElement("td");
-        let Delete=document.createElement("td");
-        id.textContent=user.id;
-        Name.textContent=user.name;
-        age.textContent=user.age;
-        designation.textContent=user.designation;
-        priority.textContent=user.priority;
-        vType.textContent=user.vaccine;
-        otp.textContent=+otpGenerator()
-        vaccine.textContent="Vaccinate";
-        vaccine.style.color="green";
-        vaccine.addEventListener("click",()=>{
-            let a =prompt("enter OTP");
-            if(a===otp.textContent){
-                alert("successfull")
-                alert(`${user.name}  Added to Queue`);
-                setTimeout(() => {
-                    alert(`Vaccinating ${user.vaccine}`);
-                  }, 5000);
-                  setTimeout(() => {
-                    alert(`${user.name} Vaccinated`);
-                  }, 10000)
-                  setTimeout(() => {
-                    vaccinatedUsers.push(user);
-                    localStorage.setItem("vaccinated",JSON.stringify(vaccinatedUsers));
-                    userData.splice(index,1);
-                    localStorage.setItem("user-data",JSON.stringify(userData));
-                    showData();
-                  }, 10005)
+let userData = JSON.parse(localStorage.getItem("user-data")) || [];
+let vaccinatedUsers = JSON.parse(localStorage.getItem("vaccinated")) || [];
+let tbody = document.getElementById("dash_Board")
+let doFilter=document.getElementById("by_vaccine");
+let doPriority=document.getElementById("by_priority");
+let doAge=document.getElementById("by_age");
 
-                
-            }else{
-                alert("wrong otp")
-            }
+doFilter.addEventListener("change",byVaccine);
+doPriority.addEventListener("change",byPriority);
+doAge.addEventListener("change",byAge);
+
+
+function showData() {
+    tbody.innerHTML = null;
+    userData.forEach(function (user, index) {
+        let tr = document.createElement("tr");
+        let id = document.createElement("td");
+        let Name = document.createElement("td");
+        let age = document.createElement("td");
+        let designation = document.createElement("td");
+        let priority = document.createElement("td");
+        let vType = document.createElement("td");
+        let otp = document.createElement("td");
+        let vaccine = document.createElement("td");
+        let Delete = document.createElement("td");
+        id.textContent = user.id;
+        Name.textContent = user.name;
+        age.textContent = user.age;
+        designation.textContent = user.designation;
+        priority.textContent = user.priority;
+        vType.textContent = user.vaccine;
+        otp.textContent = user.otp;
+        vaccine.textContent = "Vaccinate";
+        vaccine.className = "green"
+        vaccine.addEventListener("click", () => {
+            let a = prompt("enter OTP");
+
+            let promise = new Promise(function (resolve, reject) {
+                if (a === otp.textContent) {
+                    resolve("successfull")
+                }else{
+                    reject("wrong otp")
+                }
+            })
+            promise
+                .then((res)=>{
+                    alert(res)
+                    setTimeout(()=>{
+                        alert(`${user.name}  Added to Queue`);
+                    },0)
+                   
+                    setTimeout(()=>{
+                        alert(`Vaccinating ${user.vaccine}`);
+                    },5000)
+
+                    setTimeout(() => {
+                        alert(`${user.name} Vaccinated`);
+                        vaccinatedUsers.push(user);
+                        localStorage.setItem("vaccinated", JSON.stringify(vaccinatedUsers));
+                        userData.splice(index, 1);
+                        localStorage.setItem("user-data", JSON.stringify(userData));
+                        showData();
+                    }, 10000)
+
+                })
+                .catch((error)=>{
+                    alert(error);
+                })
 
         })
-        Delete.textContent="Delete";
-        Delete.style.color="red";
-        Delete.addEventListener("click",()=>{
-            userData.splice(index,1);
-            localStorage.setItem("user-data",JSON.stringify(userData));
+        Delete.textContent = "Delete";
+        Delete.className = "red"
+        Delete.addEventListener("click", () => {
+            userData.splice(index, 1);
+            localStorage.setItem("user-data", JSON.stringify(userData));
             showData();
         })
-        tr.append(id,Name,age,designation,priority,vType,otp,vaccine,Delete)
+        tr.append(id, Name, age, designation, priority, vType, otp, vaccine, Delete)
         tbody.append(tr)
-        
+
     });
 }
 
-function otpGenerator(){
-    let numbers=[1,2,3,4,5,6,7,8,9,0]
-    let str="";
-    for(let i=0;i<4;i++){
-        const randomIndex = Math.floor(Math.random() * numbers.length); 
-        const item = numbers[randomIndex];
-        str+=item;
+
+
+function byVaccine(){
+    userData = JSON.parse(localStorage.getItem("user-data"))
+      if (doFilter.value =="Covishield") {
+        userData = userData.filter((user) => user.vaccine =="Covishield")
+        console.log(userData);
+        showData()
+      } else if (doFilter.value =="Covaxin") {
+        userData = userData.filter((user) => user.vaccine =="Covaxin")
+        console.log(userData);
+        showData()
+      } else if (doFilter.value =="Sputnik") {
+        userData = userData.filter((user) => user.vaccine =="Sputnik")
+        console.log(userData);
+        showData()
+      } else {
+        userData = JSON.parse(localStorage.getItem("user-data"))
+        console.log(userData);
+        showData()
+      }
+}
+function byPriority(){
+    userData = JSON.parse(localStorage.getItem("user-data"))
+      if (doPriority.value =="p0") {
+        userData = userData.filter((user) => user.priority =="p0")
+        console.log(userData);
+        showData()
+      } else if (doPriority.value =="p1") {
+        userData = userData.filter((user) => user.priority =="p1")
+        console.log(userData);
+        showData()
+      } else if (doPriority.value =="p2") {
+        userData = userData.filter((user) => user.priority =="p2")
+        console.log(userData);
+        showData()
+      }  else if (doPriority.value =="p3") {
+        userData = userData.filter((user) => user.priority =="p3")
+        console.log(userData);
+        showData()
+      }else {
+        userData = JSON.parse(localStorage.getItem("user-data"))
+        console.log(userData);
+        showData()
+      }
+}
+function byAge(){
+    userData = JSON.parse(localStorage.getItem("user-data"))
+    if(doAge.value=="Ascending"){
+        userData=userData.sort((a,b)=>{return a.age-b.age})
+        showData()
+    }else if(doAge.value=="Descending"){
+        userData=userData.sort((a,b)=>{return b.age-a.age})
+        showData()
+    }else{
+        userData = JSON.parse(localStorage.getItem("user-data"))
+        showData()
     }
-    return str
 }
 showData();
 
